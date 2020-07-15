0f009f1c68043921787cfd27d048d73e5cbe0902
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-14 21:06:24 -0300

    renderizando elementos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import PrimeiroJSX from "./components/Primeiro"
+import Element from "./components/Elemento"
 
 function App() {
   return (
     <div className="App">
       <PrimeiroJSX />
+      <Element />
     </div>
   );
 }
