83fded83eba6561d2e9f253eaf3bf51a9a326910
unknown <ricardo.baltazar04@gmail.com>
2021-03-10 10:21:48 -0300

    iniciando novo projeto e reaproveitando repositório

--- my-app/src/components/modulo1/Clique.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Clique(a, b){
-    return(
-        alert(a + b)
-    )
-}
--- my-app/src/components/modulo1/ComponenteB.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ComponenteB(props) {
-    return (
-        <div>
-            Componente B {props.children}
-        </div>
-    )
-}
